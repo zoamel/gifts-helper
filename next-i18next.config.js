bc2cff27d3f99ff4b8ef7ce94a9d9655e764ea7a
Marcin Wojtczak <mar.wojtczak@gmail.com>
2021-10-30 22:58:47 +0200

    Change i18n config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require('path')
+
 module.exports = {
   i18n: {
     defaultLocale: 'pl',
     locales: ['pl'],
     ignoreRoutes: ['/', '/wishlist', '/shopping-list', '/users-search'],
+    localePath: path.resolve('./public/locales'),
   },
 }
