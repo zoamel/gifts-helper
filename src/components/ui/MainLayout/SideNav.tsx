ff87e20e10d8b2434dc4862fb6f37ef42471db00
Marcin Wojtczak <mar.wojtczak@gmail.com>
2021-10-31 09:43:36 +0100

    Add option to logout and login back

--- src/components/ui/MainLayout/SideNav.tsx
+++ src/components/ui/MainLayout/SideNav.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
-import { Box, VStack, Button } from '@chakra-ui/react'
-import { signOut } from 'next-auth/react'
+import { Box, VStack, Button, Flex } from '@chakra-ui/react'
+import { signIn, signOut, useSession } from 'next-auth/react'
 import { useTranslation } from 'react-i18next'
 
-import { MENU_ITEMS } from './constats'
+import { MENU_ITEMS } from './constants'
 import { MenuLink } from './MenuLink'
 import { AppLogo } from './AppLogo'
 
 export const SideNav = () => {
   const { t } = useTranslation('common')
 
+  const { data: sessionData, status } = useSession()
+
   return (
-    <Box
+    <Flex
       display="block"
       as="nav"
       flex="1 1 0%"
@@ -20,44 +22,59 @@ export const SideNav = () => {
       px={3}
       position="fixed"
       color="gray.200"
+      minHeight="full"
       sx={{
         width: 'var(--sidebar-width)',
       }}
     >
       <AppLogo />
 
-      <Box
-        overflowY="auto"
-        height="80vh"
-        minHeight="px"
-        maxHeight="full"
-        pt={5}
-        pb={6}
-      >
-        <VStack pb={6} alignItems="stretch" spacing={4}>
-          {MENU_ITEMS.map((menuItem) => (
-            <MenuLink
-              key={menuItem.label}
-              href={menuItem.href}
-              label={menuItem.label}
-              icon={menuItem.icon}
-            />
-          ))}
-        </VStack>
-      </Box>
+      <Flex overflowY="auto" minHeight="full" pt={5} pb={6} direction="column">
+        {sessionData?.user ? (
+          <>
+            <VStack pb={6} alignItems="stretch" spacing={4}>
+              {MENU_ITEMS.map((menuItem) => (
+                <MenuLink
+                  key={menuItem.label}
+                  href={menuItem.href}
+                  label={menuItem.label}
+                  icon={menuItem.icon}
+                />
+              ))}
+            </VStack>
 
-      <Box flex={1} />
+            <Box flex={1} />
 
-      <Button
-        width="full"
-        variant="outline"
-        colorScheme="white"
-        onClick={() => {
-          signOut()
-        }}
-      >
-        {t('logout')}
-      </Button>
-    </Box>
+            <Button
+              width="full"
+              variant="outline"
+              colorScheme="white"
+              isLoading={status === 'loading'}
+              onClick={() => {
+                signOut()
+              }}
+            >
+              {t('logout')}
+            </Button>
+          </>
+        ) : (
+          <>
+            <Box flex={1} />
+
+            <Button
+              width="full"
+              variant="outline"
+              colorScheme="white"
+              isLoading={status === 'loading'}
+              onClick={() => {
+                signIn()
+              }}
+            >
+              {t('login')}
+            </Button>
+          </>
+        )}
+      </Flex>
+    </Flex>
   )
 }
