f5b57eda342806c75b10416fb809e1dda90e2af5
zoamel <marcin.wojtczak@outlook.com>
2022-09-04 17:22:03 +0200

    Migrate project to railway DB

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const { i18n } = require('./next-i18next.config')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    runtime: 'experimental-edge',
+  },
   reactStrictMode: true,
   output: 'standalone',
   async redirects() {
