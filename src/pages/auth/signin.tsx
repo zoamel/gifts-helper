e4564815026fcacb3ae21982f6ba630d530f5963
zoamel <marcin.wojtczak@outlook.com>
2022-09-07 19:53:50 +0200

    Update login screen

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,6 +1,7 @@
 import { Fragment } from 'react'
 
 import {
+  AspectRatio,
   Box,
   Button,
   Center,
@@ -9,8 +10,11 @@ import {
   FormLabel,
   HStack,
   Heading,
+  Hide,
   Icon,
   Input,
+  Show,
+  Stack,
   Text,
   VStack,
 } from '@chakra-ui/react'
@@ -22,6 +26,7 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import Head from 'next/head'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
+import { AiOutlineGoogle } from 'react-icons/ai'
 import { FcGoogle } from 'react-icons/fc'
 
 import { Card } from '@/components/ui'
@@ -42,74 +47,61 @@ export default function SignIn({ providers }: Props) {
   const { t } = useTranslation('common')
 
   return (
-    <>
+    <Box height="100vh">
       <Head>
         <title>{`${t('appName')} | ${t('loginPageTitle')}`}</title>
       </Head>
 
-      <Center height="100vh" background="gray.200">
-        <Card minHeight="300px" px={0} py={6}>
-          <HStack px={8}>
-            <Icon as={GiftIcon} w={9} h={9} color="red.500" />
-            <Heading as="h1" size="xl">
-              {t('appName')}
-            </Heading>
-          </HStack>
-          <Divider
-            mt={2}
-            mb={6}
-            sx={{
-              borderBottomWidth: 1,
-              opacity: 1,
-              borderColor: 'blackAlpha.500',
-            }}
-          />
-          <HStack justifyContent="center">
-            <Box
-              width="200px"
-              height="200px"
+      <Stack height="100%" direction={{ base: 'column', md: 'row' }}>
+        <Show above="md">
+          <Center height="100%" width="50%">
+            <AspectRatio
+              width="100%"
+              maxWidth="800px"
+              ratio={4 / 3}
               position="relative"
-              mb={6}
-              rounded="50%"
-              overflow="auto"
             >
               <Image
-                src="/images/present_box.png"
+                src="/images/login_image.svg"
                 layout="fill"
-                alt="present"
+                alt="gift"
+                priority
               />
-            </Box>
-          </HStack>
-          {/* <form>
-          <VStack spacing={4} px={8}>
-            <FormControl>
-              <FormLabel>Email</FormLabel>
-              <Input type="email" disabled />
-            </FormControl>
-            <FormControl>
-              <FormLabel>Hasło</FormLabel>
-              <Input type="password" disabled />
-            </FormControl>
+            </AspectRatio>
+          </Center>
+        </Show>
 
-            <Button type="submit">Zaloguj się</Button>
-          </VStack>
-        </form>
-        <Divider
-          my={6}
-          sx={{
-            borderBottomWidth: 1,
-            opacity: 1,
-            borderColor: 'blackAlpha.500',
-          }}
-        /> */}
-          <VStack>
+        <VStack
+          width={{ base: '100%', md: '50%' }}
+          height="100%"
+          backgroundColor="blackAlpha.50"
+          justifyContent={{ base: 'flex-start', md: 'center' }}
+        >
+          <Hide above="md">
+            <Image
+              src="/images/login_image.svg"
+              width="200px"
+              height="200px"
+              alt="gift"
+              priority
+            />
+          </Hide>
+          <Heading as="h1" size="lg">
+            {t('appName')}
+          </Heading>
+          <Text pt={3}>{t('loginGreetings')}</Text>
+
+          <VStack pt={8}>
             {Object.values(providers).map((provider) => (
               <Fragment key={provider.name}>
                 {provider.name === 'Google' && (
                   <Button
+                    size="lg"
+                    variant="outline"
+                    colorScheme="red"
                     key={provider.name}
                     onClick={() => signIn(provider.id, { callbackUrl })}
-                    leftIcon={<FcGoogle />}
+                    leftIcon={<Icon as={AiOutlineGoogle} />}
                   >
                     <Center>
                       <Text>{t('loginWithGoogle')}</Text>
@@ -119,9 +111,9 @@ export default function SignIn({ providers }: Props) {
               </Fragment>
             ))}
           </VStack>
-        </Card>
-      </Center>
-    </>
+        </VStack>
+      </Stack>
+    </Box>
   )
 }
 
