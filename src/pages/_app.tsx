d0b331ed1f5a10b098ddb97997b326735a432104
Marcin Wojtczak <mar.wojtczak@gmail.com>
2022-09-20 23:08:01 +0200

    Fix _app ts errors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,18 +5,25 @@ import '@fontsource/open-sans/600.css'
 import '@fontsource/open-sans/700.css'
 import '@fontsource/raleway/400.css'
 import {
+  DehydratedState,
   Hydrate,
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
-import 'focus-visible/dist/focus-visible'
+import { Session } from 'next-auth'
 import { SessionProvider } from 'next-auth/react'
 import { appWithTranslation } from 'next-i18next'
 import type { AppProps } from 'next/app'
 
 import theme from '../theme'
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{
+  session: Session
+  dehydratedState?: DehydratedState
+}>) {
   const [queryClient] = React.useState(() => new QueryClient())
 
   return (
@@ -32,4 +39,5 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   )
 }
 
+// @ts-ignore
 export default appWithTranslation(MyApp)
